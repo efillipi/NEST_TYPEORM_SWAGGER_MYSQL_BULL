5cbc8f254d3347cc38cebd2cb92dc746bcd9a1fb
Edney Fillipi <e.fillipi@gmail.com>
2022-03-11 12:27:22 -0300

    :sparkles: init tests

---
+++ jest.config.js
@@ -0,0 +1,27 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+module.exports = {
+  moduleDirectories: ['node_modules', 'src'],
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  roots: ['src'],
+  testMatch: ['**/*.spec.ts'],
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    '<rootDir>/src/modules/**/controllers/**/*.ts',
+    '<rootDir>/src/modules/**/services/**/*.ts',
+    '<rootDir>/src/shared/providers/**/*.ts',
+    '<rootDir>/src/shared/jobs/**/*.ts',
+  ],
+  clearMocks: true,
+  coverageReporters: ['text-summary', 'lcov'],
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    'src/(.*)': '<rootDir>/src/$1',
+  },
+};
